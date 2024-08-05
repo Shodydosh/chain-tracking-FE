9397f9d29d0b42c646df81a82aa67600aad7c487
Tung Nguyen <shodydosh@gmail.com>
2024-08-06 03:03:59 +0700

    done tailwind css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,21 @@
 function Header() {
-    return (
-      <div
-        style={{
-          backgroundColor: "#1a1a1a",
-          borderRadius: "10px",
-          minHeight: "220px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: "100px"
-        }}
-      >
-        <div>
-          <h3>Header</h3>
-        </div>
+  return (
+    <div
+      style={{
+        backgroundColor: '#1a1a1a',
+        borderRadius: '10px',
+        minHeight: '220px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '100px',
+      }}
+    >
+      <div className="bg-pink-200">
+        <h3>Header</h3>
       </div>
-    );
-  }
-  
-  export default Header;
-  
+    </div>
+  );
+}
+
+export default Header;
